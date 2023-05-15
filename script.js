bbfd23fe2c14b5ff911dc1fd7b644124caf69dd5
Gianluca Paschoalotti <gianluca170703@gmail.com>
2023-05-14 21:49:50 -0300

    primeiro commit do projeto

---
+++ script.js
@@ -0,0 +1,46 @@
+/* const animais= document.getElementById('animais');
+console.log(animais);
+
+const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection[0]);
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
+
+const primeiraUl = document.querySelector('ul');
+console.log(primeiraUl);
+
+const linksInternos = document.querySelector('[herf^="#"]');
+console.log(linksInternos);
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg[1]);
+
+const gridSectionHTML = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('.grid-section');
+
+console.log(gridSectionHTML);
+console.log(gridSectionNode);
+
+gridSectionNode.forEach(function(item, index){
+    console.log(index);
+})
+
+const arrayGrid = Array.from(gridSectionHTML);
+
+console.log(arrayGrid) */
+
+/* const imagens = document.querySelectorAll('img');
+console.log(imagens); - correta */
+
+/* const imagensImg = document.querySelectorAll('img[src^="img/imagem"]');
+console.log(imagensImg); - Corrigido */
+
+/* const linksInternos = document.querySelectorAll('[href^="#"]');
+console.log(linksInternos); - Correta */
+
+/* const h2Animais = document.querySelectorAll('.animais-descricao h2');
+console.log(h2Animais[0]); - Correta porém metodo diferente */
+
+const pSite = document.querySelectorAll('p');
+console.log(pSite[18])
